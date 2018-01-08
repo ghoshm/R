e030a7a5b075f57a3a2b9de590aafdee38ab248a
Marcus <ghoshm@hotmail.co.uk>
2018-01-08 18:01:28 +0000

    Working

--- Clustering.R
+++ Clustering.R
@@ -7,12 +7,12 @@ library(tictoc)
 memory.limit(size = 16284) # increase memory
 
 # Loading Data from Mat Files 
-data <- readMat('C:\\Users\\Marcus\\Desktop\\Sample_full.mat')
+data <- readMat('C:\\Users\\Marcus\\Desktop\\sample.mat') # Note you need double slashes 
 gc() # reduce memory usage 
 
 # Clustering using Density Cut 
 tic()
-cluster.out = DensityCut(matrix(data$sample[1:6000000,1]))
+cluster.out = DensityCut(matrix(data$sample))
 toc() 
 
 # Useful commands 
